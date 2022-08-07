286c61f4d4d291ef885ec7b018aa550961b78cf7
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-08-07 15:14:50 -0400

    💣 CODIFICAÇÃO - Iniciando consumo da API

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,6 +2,37 @@ import { useEffect, useState } from "react";
 
 const LandingPage = () => {
 
+    const [dataProduct, setDataProduct] = useState([] as any)
+
+    const getProduct = async () => {
+
+        const url = 'https://api.mercadolibre.com/sites/MLB/search?category=categoryId&q=celular#json'
+        
+        await fetch(url)
+            .then(async function(response){
+                const data = await response.json()
+                const results = data.results
+
+                results.forEach( (product: any) => {               
+                    setDataProduct((currentList: any) => [...currentList, product])
+                    console.log(dataProduct);
+                })
+
+                // return console.log('Dados da API recebidos com sucesso!');
+                
+                
+            })
+            .catch(function(error){
+                return console.log('Falha no recebimento dos dados da API. Erro ' + error.message);
+                
+            })
+        
+    }
+
+    useEffect(() => {
+        getProduct();
+    }, [])
+
     return (
         <>
             <header>
@@ -19,7 +50,6 @@ const LandingPage = () => {
             </header>
             <section>
                 <aside className="asideFilters">
-
                 </aside>
                 <aside className="asideExbProducts"></aside>
             </section>
